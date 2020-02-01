d7744881a58e9a67c0e2d15ff9269892401b2964
xJ4111 <e3arul@gmail.com>
2020-02-01 12:43:56 +0000

    Player Prefab + Player Test Scene

---
+++ Fix Me/Assets/Scripts/swap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swap : MonoBehaviour
+{
+    public GameObject a, b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            a.SetActive(!a.activeSelf);
+            b.SetActive(!b.activeSelf);
+        }
+    }
+}
